80777ef97f36807d867b2d71385bf9d54545d15a
siffatkhan <siffatullah.321@example.com>
2025-07-01 12:54:36 +0500

    NavLinks Added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,37 +6,37 @@ const Navbar = () => {
     <nav className="border-t border-b border-black flex items-center justify-between px-5 py-2">
       <ul className="flex space-x-4">
         <li>
-          <NavLink to="/" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive?"highlight":""}} to="/" className="px-3 py-1">
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/journals" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/journals" className="px-3 py-1">
             Journals
           </NavLink>
         </li>
         <li>
-          <NavLink to="/past-papers" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/past-papers" className="px-3 py-1">
             Papers
           </NavLink>
         </li>
         <li>
-          <NavLink to="/gpa-calculator" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/gpa-calculator" className="px-3 py-1">
             GPA Calculator
           </NavLink>
         </li>
         <li>
-          <NavLink to="/about" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/about" className="px-3 py-1">
             About
           </NavLink>
         </li>
         <li>
-          <NavLink to="/contact" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/contact" className="px-3 py-1">
             Contact
           </NavLink>
         </li>
         <li>
-          <NavLink to="/login" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive?  "highlight":""}} to="/login" className="px-3 py-1">
             Login
           </NavLink>
         </li>
