53a9389395a6997bc7c55f9fbcea425336b807f0
siffatkhan <siffatullah.321@example.com>
2025-07-01 13:01:59 +0500

    NavLink Classes Added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,37 +6,38 @@ const Navbar = () => {
     <nav className="border-t border-b border-black flex items-center justify-between px-5 py-2">
       <ul className="flex space-x-4">
         <li>
-          <NavLink className={(e)=>{return e.isActive?"highlight":""}} to="/" className="px-3 py-1">
+          {/* Removed Classname from home becasuse i dont want it to highlight all tume */}
+          <NavLink  to="/" >  
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/journals" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/journals" >
             Journals
           </NavLink>
         </li>
         <li>
-          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/past-papers" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/past-papers" >
             Papers
           </NavLink>
         </li>
         <li>
-          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/gpa-calculator" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/gpa-calculator" >
             GPA Calculator
           </NavLink>
         </li>
         <li>
-          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/about" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/about" >
             About
           </NavLink>
         </li>
         <li>
-          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/contact" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive? "highlight":""}} to="/contact" >
             Contact
           </NavLink>
         </li>
         <li>
-          <NavLink className={(e)=>{return e.isActive?  "highlight":""}} to="/login" className="px-3 py-1">
+          <NavLink className={(e)=>{return e.isActive?  "highlight":""}} to="/login">
             Login
           </NavLink>
         </li>
