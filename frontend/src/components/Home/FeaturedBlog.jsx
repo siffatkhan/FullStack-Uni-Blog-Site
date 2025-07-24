49611aef8010be9b03c28c2389ad2e25388876d7
siffatkhan <siffatullah.321@gmail.com>
2025-07-24 12:27:33 +0500

    Frontend Work

--- frontend/src/components/Home/FeaturedBlog.jsx
+++ frontend/src/components/Home/FeaturedBlog.jsx
@@ -8,24 +8,30 @@ const FeaturedBlog = () => {
       <p className="featuredLabel">FEATURED POST</p>
        
       <div className="blogCard">
-        <img src="./sifatPic.png" alt="About Blog Pic" className="blogPic" />
+        <img src="./featured.jpg" alt="About Blog Pic" className="blogPic" />
         
         <div className="blogContent">
           <div className="blogMeta">
             <p className="blogAuthor">Admin</p>
-            <p className="blogDate">July 20, 2025 - 1 min read</p>
+            <p className="blogDate">July 20, 2025 <span className="dot">•</span> 1 min read</p>
           </div>
 
+          <div className="content">
           <Link to="/" className="blogLink">
             <h1 className="blogTitle">Why I made this</h1>
             <p className="blogExcerpt">
               The sole purpose of this website, uploading blogs, past papers, and everything was...
             </p>
           </Link>
+          </div>
         </div>
       </div>
     </div>
+ 
   )
 }
 
 export default FeaturedBlog
+
+
+
